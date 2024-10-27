62a1ed349d6c8af869bc6bc577ea38f14b72b3f8
Max <152314795+cptzlp@users.noreply.github.com>
2024-10-27 23:43:46 +0300

    Add files via upload
    firstCommit

---
+++ spring-mvcJR/src/main/java/ru/max/springcourse/springmvcjr/SpringMvcJrApplication.java
@@ -0,0 +1,13 @@
+package ru.max.springcourse.springmvcjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcJrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcJrApplication.class, args);
+    }
+
+}
---
+++ spring-mvcJR/src/main/java/ru/max/springcourse/springmvcjr/model/Client.java
@@ -0,0 +1,15 @@
+package ru.max.springcourse.springmvcjr.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Client {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+
+}
